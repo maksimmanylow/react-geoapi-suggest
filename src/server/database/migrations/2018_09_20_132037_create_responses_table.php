fd4ab45c50bfe8c038bb96bead63da288d2e3083
Maksim Manylow <maksimmanylow@gmail.com>
2018-09-20 16:31:32 +0300

    +migrations +models +route

--- src/server/database/migrations/2018_09_20_132037_create_responses_table.php
+++ src/server/database/migrations/2018_09_20_132037_create_responses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateResponsesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('responses', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('suggestion');
+            $table->unsignedInteger('request_id');
             $table->timestamps();
+
+            $table->foreign('request_id')
+              ->references('id')->on('requests')
+              ->onDelete('cascade');
         });
     }
 
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('responses');
     }
 }
